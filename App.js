cb4deddb8388bd41925655e4ae936d87935de090
Emiliux159 <93285312+Emiliux159@users.noreply.github.com>
2022-06-03 13:57:37 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler'
+import  * as React from 'react';
+import {NavigationContainer}from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from "./screens/Home";
+import IssLocationScreen from "./screens/IssLocation";
+import MeteorScreen from "./screens/Meteors";
+import UpdateScreen from "./screens/Updates";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+       <IssLocationScreen></IssLocationScreen>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
